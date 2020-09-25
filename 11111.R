8376a2c090315d9f378d9ffc31c21fa7cf867405
jassss-pan <minhui.pan@mail.utoronto.ca>
2020-09-25 05:11:42 +0000

    first commit

---
+++ 11111.R
@@ -0,0 +1,34 @@
+install.packages("tidyverse")
+
+install.packages("blogdown")
+
+library(blogdown)
+
+blogdown::new_site(
+  dir = ".",
+  install_hugo = TRUE,
+  format = "toml",
+  sample = TRUE,
+  theme = "yihui/hugo-lithium",
+  hostname = "github.com",
+  theme_example = TRUE,
+  empty_dirs = FALSE,
+  to_yaml = TRUE,
+  serve = interactive()
+)
+
+
+blogdown::new_post(
+  "page 2",
+  kind = "",
+  open = interactive(),
+  author = getOption("blogdown.author"),
+  categories = NULL,
+  tags = NULL,
+  date = Sys.Date(),
+  file = NULL,
+  slug = NULL,
+  title_case = getOption("blogdown.title_case"),
+  subdir = getOption("blogdown.subdir", "post"),
+  ext = getOption("blogdown.ext", ".md")
+)
